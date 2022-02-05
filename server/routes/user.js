69c5874a097533464b4698777d7ce26f435dde5c
meashishpokhrel <meashishpokhrel@gmail.com>
2022-02-05 22:06:46 +0545

    user Schema Created and express validator added

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,20 @@
 const express = require("express")
 
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
+const User = require("../models/User")
 // api/signup
 //Registering a User
-router.post("/", (req,res)=> {
-    res.send("Register a User !")
+router.post("/", 
+body('email').isEmail(),
+body('password').isLength({ min: 5 }),
+(req,res)=> {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        return res.status(400).json({err: errors.array()})
+    }
+   res.send("Coorect")
 })
 
 module.exports = router
