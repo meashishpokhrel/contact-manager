c6a8d3b6b76ef00a1606b4595d539d539f110b61
meashishpokhrel <meashishpokhrel@gmail.com>
2022-02-07 12:20:30 +0545

    Store connected with App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import "./assets/styles/App.scss";
 import Contact from "./component/contact/Contact";
 import Navbar from "./component/layout/Navbar";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Contact />
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Contact />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
