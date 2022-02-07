ea9d106b39b2a57134545e43aa08cc6a08f09116
meashishpokhrel <meashishpokhrel@gmail.com>
2022-02-07 23:41:42 +0545

    Validation Error SOlved

--- client/src/component/contact/Contact.js
+++ client/src/component/contact/Contact.js
@@ -8,37 +8,35 @@ const Contact = ({ contact }) => {
   const { id } = contact;
   const dispatch = useDispatch();
   return (
-    <div>
-      <tr>
-        <th scope="row">{contact.id}</th>
-        <td>
-          <Avatar src={contact.photo} size="38" round={true} /> {contact.name}
-        </td>
-        <td>{contact.email}</td>
-        <td>{contact.phone}</td>
-        <td>{contact.address}</td>
-        <td>
-          <div className="custom-control custom-checbox">
-            <input type="checkbox" className="custom-control-input" />
-            <label className="custom-control-label"></label>
-          </div>
-        </td>
-        <td>
-          <Link to={`/contact/edit/${contact.id}`}>
-            <span className="material-icons">edit</span>
-          </Link>
-        </td>
-        <td>
-          <span
-            type="button"
-            className="material-icons text-danger"
-            onClick={() => dispatch(deleteContact(id))}
-          >
-            delete
-          </span>
-        </td>
-      </tr>
-    </div>
+    <tr>
+      <th scope="row">{contact.id}</th>
+      <td>
+        <Avatar src={contact.photo} size="38" round={true} /> {contact.name}
+      </td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+      <td>{contact.address}</td>
+      <td>
+        <div className="custom-control custom-checbox">
+          <input type="checkbox" className="custom-control-input" />
+          <label className="custom-control-label"></label>
+        </div>
+      </td>
+      <td>
+        <Link to={`/contact/edit/${contact.id}`}>
+          <span className="material-icons">edit</span>
+        </Link>
+      </td>
+      <td>
+        <span
+          type="button"
+          className="material-icons text-danger"
+          onClick={() => dispatch(deleteContact(id))}
+        >
+          delete
+        </span>
+      </td>
+    </tr>
   );
 };
 
