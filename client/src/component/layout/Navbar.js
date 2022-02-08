9189b4326d1167240edf01de4e0a4724f2401b90
meashishpokhrel <meashishpokhrel@gmail.com>
2022-02-08 21:28:29 +0545

    Navbar Complete

--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signOut } from "../../redux/actions/auth.action";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -10,28 +11,40 @@ const Navbar = () => {
     dispatch(signOut());
     navigate("/");
   };
+
+  const auth = useSelector((state) => state.auth);
+
   return (
     <nav className="navar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
       <div className="container">
-        <Link to="/contact" className="navbar-brand">
-          Contact Manager
-        </Link>
-
-        <Link to="/signin" className="btn btn-warning">
-          Sigin
-        </Link>
-
-        <Link to="/signup" className="btn btn-warning">
-          Signup
-        </Link>
-
-        <button className="btn btn-warning" onClick={(e) => handleSignOut(e)}>
-          Signout
-        </button>
-
-        <Link to="/contact/add" className="btn btn-warning">
-          Add Contacts
-        </Link>
+        {auth.token ? (
+          <>
+            <Link to="/contact" className="navbar-brand">
+              Contact Manager
+            </Link>
+            <Link to="/contact/add" className="btn btn-warning">
+              Add Contacts
+            </Link>
+            <button
+              className="btn btn-warning"
+              onClick={(e) => handleSignOut(e)}
+            >
+              Signout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/contact" className="navbar-brand">
+              Contact Manager
+            </Link>
+            <Link to="/signin" className="btn btn-warning">
+              Sigin
+            </Link>
+            <Link to="/signup" className="btn btn-warning">
+              Signup
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
